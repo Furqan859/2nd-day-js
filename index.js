6d76b5b4632180b3572445d81f11e1eb64b64df4
Furqan Rasool <65628215+Furqan859@users.noreply.github.com>
2021-12-10 22:26:21 +0500

    i have done the random color changing in js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-var button = document.getElementById('btn-id');
+var btn = document.getElementById('btn-id');
+var mainDiv = document.querySelector('body');
 
-button.onclick = function(){
-
-    alert('button is clicked');
+function onBtnclick (){
     
+    mainDiv.style.backgroundColor ="rgb(" + Math.floor(Math.random()*255)+ "," + Math.floor(Math.random()*255) +"," + Math.floor(Math.random()*255) + ")"
 }
+
+// btn.onclick = onbtnclick;
+
+btn.addEventListener('click',onBtnclick);
